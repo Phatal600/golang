6273fc8c745f53f3526bf2869f96eab93034368d
Albert White <awhite@deloitte.com>
2023-04-09 08:25:22 -0400

    deck.go

--- go.mod
+++
@@ -1,6 +0,0 @@
-module example
-
-go 1.20
-
-require rsc.io/quote v1.5.2
-
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	cards := newDeck()
+	cards.saveToFile("my_cards")
+	//fmt.Println(cards.toString())
+	//	hand, remainingCards := deal(cards, 5)
+	//	hand.print()
+	//	remainingCards.print()
+	//
+}
